7023ff296ec5be6b8f6f83c308d1c6aad0f4649e
f <f@DESKTOP-B06DPQM>
2017-02-06 21:56:06 +0800

    添加项目文件。

---
+++ ainoui/xiangqi.h
@@ -0,0 +1,15 @@
+#pragma once
+#ifndef XIANGQI_H
+#define XIANGQI_H
+#define IDR_CLICK      "click"
+#define IDR_ILLEGAL    "illeagal"
+#define IDR_MOVE       "move"
+#define IDR_MOVE2      "move2"
+#define IDR_CAPTURE    "capture"
+#define IDR_CAPTURE2   "capture2"
+#define IDR_CHECK      "check"
+#define IDR_CHECK2     "check2"
+#define IDR_WIN        "win"
+#define IDR_DRAW       "draw"
+#define IDR_LOSS       "loss"
+#endif
